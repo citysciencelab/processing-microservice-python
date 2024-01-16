37b9ad82f5bb52cc783ed92dba6b20c693d59db5
herzogrh <rico.herzog@outlook.com>
2024-01-16 17:10:33 +0100

    Async tryout

--- NetLogo/ProcessManager.py
+++ NetLogo/ProcessManager.py
@@ -1,19 +1,22 @@
 from NetLogo.ProcessMetadata import NETLOGO_VARIABLES, NETLOGO_REPORTERS
+from importlib import reload
 
-    
+import pynetlogo, pandas as pd
 
+netlogo = pynetlogo.NetLogoLink(gui=False)
+netlogo.load_model("./NetLogo/model/Model_Rothenburgsort.nlogo")
 
-def simulateResults(inputParameters, netlogo):
-
-    import pynetlogo, pandas as pd
+async def simulate_results(inputParameters):
 
     # For Linux environments
     # netlogo = pynetlogo.NetLogoLink(gui=False, netlogo_home='../../NetLogo 6.3.0')
 
     # For Mac environments
-    netlogo = pynetlogo.NetLogoLink(gui=False)
+    # netlogo = pynetlogo.NetLogoLink(gui=False, jvm_path=jvm_path)
 
     netlogo.load_model("./NetLogo/model/Model_Rothenburgsort.nlogo")
+
+    finalResults = None
 
     try:
         parameters = {NETLOGO_VARIABLES[key]: value for key, value in inputParameters.items()}
@@ -31,10 +34,12 @@ def simulateResults(inputParameters, netlogo):
 
         results = pd.DataFrame(results)
 
-        netlogo.kill_workspace()
-
-        return results.to_json(orient='records')
+        finalResults = results.to_json(orient='records')
     
     except Exception as e:
         print(f"An error occurred: {str(e)}")
-        return None
+    
+    finally:
+        netlogo.kill_workspace()
+
+        return finalResults
--- server.py
+++ server.py
@@ -1,12 +1,16 @@
-import socketio, json
+import socketio, json, threading, pandas as pd
+from NetLogo.ProcessMetadata import NETLOGO_VARIABLES, NETLOGO_REPORTERS
+
+from importlib import reload
+import pynetlogo
 
 from config import URL, PROCESS_METADATA
-
-netlogo = None
 
 # Create a SocketIO client
 sio = socketio.Client()
 
+jvm_path = None
+print("Main class thread ID:", threading.get_ident())
 
 
 # Function to handle SocketIO events
@@ -25,10 +29,13 @@ def disconnect():
 # Function to handle "execute" event
 @sio.event
 def execute(data):
+    global netlogo
+    netlogo = pynetlogo.NetLogoLink(gui=False)
+    netlogo.load_model("NetLogo/model/Model_Rothenburgsort.nlogo")
 
-    from NetLogo.ProcessManager import simulateResults
+    final_results = None
 
-    # Check if there is data
+     # Check if there is data
     if not data:
         print("No data provided - Job", data["jobID"], "aborted")
         return
@@ -41,20 +48,53 @@ def execute(data):
     data.pop("jobID", None)
     inputParameters = data
 
-    print("Input parameters:", inputParameters)
+    try:
+        parameters = {NETLOGO_VARIABLES[key]: value for key, value in inputParameters.items()}
 
-    results = simulateResults(inputParameters, netlogo)
+        for key, value in parameters.items():
+            # If value is a string, it needs to be wrapped in quotes
+            if isinstance(value, str):
+                value = f'"{value}"'
+
+            netlogo.command(f'set {key} {value}')
+
+        netlogo.command('setup')
+
+        results = netlogo.repeat_report(NETLOGO_REPORTERS, parameters['Simulationszeit'] * 12, go="go")
+
+        results = pd.DataFrame(results)
+
+        final_results = results.to_json(orient='records')
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+    finally:
+        print(final_results)
+    
+
+    #print("Current thread ID:", threading.get_ident())
+    #print("Main thread ID:", threading.main_thread().ident)
+
+    # print("Input parameters:", inputParameters)
+
+    print("JVM path:", jvm_path)
+
+    #results = NetLogo.simulate_results(inputParameters)
 
     response = {
         "jobID": jobID,
         "processID": PROCESS_METADATA["id"],
-        "results": results,
+        "results": final_results,
         "mimetype": "application/json"
     }
 
     sio.emit("simulation_results", response)
 
     print("Job execution finished (Job ID", jobID, ")")
+
+    #jpype.java.lang.Thread.detach()
+    netlogo.kill_workspace()
 
 # Function to handle "registration_success" event
 @sio.on("registration_success")
@@ -67,6 +107,7 @@ if __name__ == "__main__":
     try:
         sio.connect(URL)
         sio.wait()
+        
     except Exception as e:
         print("An exception occurred:", str(e))
         # Log the exception here
