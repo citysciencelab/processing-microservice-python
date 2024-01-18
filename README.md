e61af3efb12a341e75ff54a0b33ffce2c4b30eed
herzogrh <rico.herzog@outlook.com>
2024-01-18 13:12:09 +0100

    Refactor to async server

--- README.md
+++ README.md
@@ -16,8 +16,8 @@ All of the necessary configuration is done in the [config.py](./config.py) file.
 Although one can use basically any algorithm to handle the input, do calculations and then return an output, there are multiple simulation packages written in Python that one can leverage. Thus, multiple extensions come along with this processing microservice that one can use out of the box. The following extensions are currently available: 
 
 ### NetLogo
-[NetLogo](https://github.com/citysciencelab/processing-microservice-nodejs/) is one of the most popular agent-based modelling softwares. Build on Java, models are written in a specific NetLogo language. To simulate a NetLogo model with the processing microservice, ```from NetLogo.ProcessManager import simulateResults``` in the ```server.py``` file. Create or download the NetLogo, save it in the ```NetLogo/model```folder and specify the correct NetLogo input variables and reporters in the ```NetLogo/ProcessMetadata.py``` file. 
-
+[NetLogo](https://github.com/citysciencelab/processing-microservice-nodejs/) is one of the most popular agent-based modelling softwares. Build on Java, models are written in a specific NetLogo language. With the help of the great [pynetlogo](https://pynetlogo.readthedocs.io/en/latest/) library, this Python processing microservice can run NetLogo models. 
+To simulate a NetLogo model with the processing microservice, ```import NetLogo.ProcessManager as NetLogo``` in the ```server.py``` file and ```await NetLogo.simulate_results(inputParameters)``` in the execute event. Create or download the NetLogo model, save it in the ```NetLogo/model```folder and specify the correct NetLogo input variables and reporters in the ```NetLogo/ProcessMetadata.py``` file. 
 
 ## Other Processing Microservices
 - [node.js Processing Microservice](https://github.com/citysciencelab/processing-microservice-nodejs/)
--- server.py
+++ server.py
@@ -1,113 +1,66 @@
-import socketio, json, threading, pandas as pd
-from NetLogo.ProcessMetadata import NETLOGO_VARIABLES, NETLOGO_REPORTERS
-
-from importlib import reload
-import pynetlogo
-
+import asyncio
+import socketio
 from config import URL, PROCESS_METADATA
 
-# Create a SocketIO client
-sio = socketio.Client()
+import NetLogo.ProcessManager as NetLogo
 
-jvm_path = None
-print("Main class thread ID:", threading.get_ident())
+async def main():
+    # Create a Socket.IO client instance
+    sio = socketio.AsyncClient()
 
+    # Define event handlers
+    @sio.event
+    async def connect():
+        print('Connected to server')
+        await sio.emit('register', PROCESS_METADATA)
 
-# Function to handle SocketIO events
-@sio.event
-def connect():
-    print("SocketIO connection established")
+    @sio.event
+    async def disconnect():
+        print('Disconnected from server')
 
-    sio.emit('register', PROCESS_METADATA)
-
-
-@sio.event
-def disconnect():
-    print("SocketIO connection closed")
-
-
-# Function to handle "execute" event
-@sio.event
-def execute(data):
-    global netlogo
-    netlogo = pynetlogo.NetLogoLink(gui=False)
-    netlogo.load_model("NetLogo/model/Model_Rothenburgsort.nlogo")
-
-    final_results = None
-
-     # Check if there is data
-    if not data:
-        print("No data provided - Job", data["jobID"], "aborted")
-        return
-
-    # Job ID
-    jobID = data["jobID"]
-
-    print("Job execution started (Job ID", jobID, ")")
-
-    data.pop("jobID", None)
-    inputParameters = data
-
-    try:
-        parameters = {NETLOGO_VARIABLES[key]: value for key, value in inputParameters.items()}
-
-        for key, value in parameters.items():
-            # If value is a string, it needs to be wrapped in quotes
-            if isinstance(value, str):
-                value = f'"{value}"'
-
-            netlogo.command(f'set {key} {value}')
-
-        netlogo.command('setup')
-
-        results = netlogo.repeat_report(NETLOGO_REPORTERS, parameters['Simulationszeit'] * 12, go="go")
-
-        results = pd.DataFrame(results)
-
-        final_results = results.to_json(orient='records')
-
-    except Exception as e:
-        print(f"An error occurred: {str(e)}")
-
-    finally:
-        print(final_results)
     
+    @sio.on("registration_success")
+    def registration_success(data):
+        print("registration_success", data)
 
-    #print("Current thread ID:", threading.get_ident())
-    #print("Main thread ID:", threading.main_thread().ident)
 
-    # print("Input parameters:", inputParameters)
+    # Function to handle "execute" event
+    @sio.event
+    async def execute(data):
 
-    print("JVM path:", jvm_path)
+        # Check if there is data
+        if not data:
+            print("No data provided - Job", data["jobID"], "aborted")
+            return
 
-    #results = NetLogo.simulate_results(inputParameters)
+        # Job ID
+        jobID = data["jobID"]
 
-    response = {
-        "jobID": jobID,
-        "processID": PROCESS_METADATA["id"],
-        "results": final_results,
-        "mimetype": "application/json"
-    }
+        print("Job execution started (Job ID", jobID, ")")
 
-    sio.emit("simulation_results", response)
+        data.pop("jobID", None)
+        inputParameters = data        
 
-    print("Job execution finished (Job ID", jobID, ")")
+        # print("Input parameters:", inputParameters)
 
-    #jpype.java.lang.Thread.detach()
-    netlogo.kill_workspace()
+        results = await NetLogo.simulate_results(inputParameters)
 
-# Function to handle "registration_success" event
-@sio.on("registration_success")
-def registration_success(data):
-    print("registration_success", data)
+        response = {
+            "jobID": jobID,
+            "processID": PROCESS_METADATA["id"],
+            "results": results,
+            "mimetype": "application/json"
+        }
 
-# Connect to the SocketIO server
-if __name__ == "__main__":
+        await sio.emit("simulation_results", response)
 
-    try:
-        sio.connect(URL)
-        sio.wait()
-        
-    except Exception as e:
-        print("An exception occurred:", str(e))
-        # Log the exception here
+        print("Job execution finished (Job ID", jobID, ")")
+
+    # Connect to the server
+    await sio.connect(URL)
+
+    # Wait for the connection to be established
+    await sio.wait()
+
+# Run the main function
+asyncio.run(main())
