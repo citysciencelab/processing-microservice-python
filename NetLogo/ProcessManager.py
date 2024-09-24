fbfa80931700289a13a68679b5f9d341917de82d
herzogrh <rico.herzog@outlook.com>
2024-09-24 17:43:40 +0200

    Logging improved

--- NetLogo/ProcessManager.py
+++ NetLogo/ProcessManager.py
@@ -1,20 +1,22 @@
 from NetLogo.ProcessMetadata import NETLOGO_VARIABLES, NETLOGO_REPORTERS
 
-import pynetlogo, json, pandas as pd, geopandas as gpd
+import pynetlogo, json, pandas as pd, geopandas as gpd, logging
 
 # For Mac environments
 # netlogo = pynetlogo.NetLogoLink(gui=False)
 
 # For Linux environments
-netlogo = pynetlogo.NetLogoLink(gui=False, netlogo_home="../../NetLogo 6.3.0")
-
-netlogo.load_model("./NetLogo/model/Model_Rothenburgsort.nlogo")
 
 
-Rothenburgsort = gpd.read_file("./NetLogo/model/data/rothenburgsort.json")
-
-
+@staticmethod
 async def simulate_results(inputParameters):
+
+    netlogo = pynetlogo.NetLogoLink(gui=False, netlogo_home="../../NetLogo 6.3.0")
+
+    netlogo.load_model("./NetLogo/model/Model_Rothenburgsort.nlogo")
+
+
+    Rothenburgsort = gpd.read_file("./NetLogo/model/data/rothenburgsort.json")
 
     finalResults = None
 
@@ -22,6 +24,9 @@ async def simulate_results(inputParameters):
         parameters = {
             NETLOGO_VARIABLES[key]: value for key, value in inputParameters.items()
         }
+
+        logging.debug("Parameters:")
+        logging.debug(parameters)
 
         for key, value in parameters.items():
             # If value is a string, it needs to be wrapped in quotes
@@ -31,6 +36,8 @@ async def simulate_results(inputParameters):
             netlogo.command(f"set {key} {value}")
 
         netlogo.command("setup")
+
+        logging.debug(netlogo)
 
         results = netlogo.repeat_report(
             list(NETLOGO_REPORTERS.values()),
@@ -58,5 +65,5 @@ async def simulate_results(inputParameters):
         print(f"An error occurred: {str(e)}")
 
     finally:
-        netlogo.kill_workspace()
+       # netlogo.kill_workspace()
         return finalResults
--- server.py
+++ server.py
@@ -1,70 +1,85 @@
-import asyncio
-import socketio
+import asyncio, socketio, logging
 from config import URL, PROCESS_METADATA
 
 import NetLogo.ProcessManager as NetLogo
 #import Mesa.ProcessManager as Mesa
 
+logger = True
+engineio_logger = True
+logging.basicConfig(level=logging.DEBUG)
+
+sio = socketio.AsyncClient(logger=logger, engineio_logger=engineio_logger)
+
 async def main():
     # Create a Socket.IO client instance
-    logger = True
-    engineio_logger = True
-
-    sio = socketio.AsyncClient(logger=logger, engineio_logger=engineio_logger)
-
-    # Define event handlers
-    @sio.event
-    async def connect():
-        print('Connected to server')
-        await sio.emit('register', PROCESS_METADATA)
-
-    @sio.event
-    async def disconnect():
-        print('Disconnected from server')
-
-    
-    @sio.on("registration_success")
-    def registration_success(data):
-        print("registration_success", data)
-
-
-    # Function to handle "execute" event
-    @sio.event
-    async def execute(data):
-
-        # Check if there is data
-        if not data:
-            print("No data provided - Job", data["jobID"], "aborted")
-            return
-
-        # Job ID
-        jobID = data["jobID"]
-
-        print("Job execution started (Job ID", jobID, ")")
-
-        data.pop("jobID", None)
-        inputParameters = data        
-
-        # print("Input parameters:", inputParameters)
-
-        results = await NetLogo.simulate_results(inputParameters)
-
-        response = {
-            "jobID": jobID,
-            "processID": PROCESS_METADATA["id"],
-            "results": results,
-            "mimetype": "application/json"
-        }
-
-        await sio.emit("simulation_results", response)
-
-        print("Job execution finished (Job ID", jobID, ")")
-
     # Connect to the server
     await sio.connect(URL)
 
     # Wait for the connection to be established
     await sio.wait()
 
+# Define event handlers
+@sio.event
+async def connect():
+    print('Connected to server')
+    await sio.emit('register', PROCESS_METADATA)
+
+@sio.event
+async def disconnect():
+    print('Disconnected from server')
+
+
+@sio.on("registration_success")
+def registration_success(data):
+    print("registration_success", data)
+
+
+# Function to handle "execute" event
+@sio.event
+async def execute(data):
+
+    # Check if there is data
+    if not data:
+        print("No data provided - Job", data["jobID"], "aborted")
+        return
+
+    # Job ID
+    jobID = data["jobID"]
+
+    logging.debug("Job execution started")
+    logging.debug(jobID)
+
+    data.pop("jobID", None)
+    inputParameters = data        
+
+    # print("Input parameters:", inputParameters)
+    results = "Dummy results"
+
+    try:
+        loop = asyncio.get_event_loop()
+        task = loop.create_task(NetLogo.simulate_results(inputParameters))
+        logging.debug("Task created")
+        logging.debug(task)
+        results = await task
+        logging.debug("Results:")
+        logging.debug(results)
+    except Exception as e:
+        logging.error(f"An error occurred while simulating results: {e}")
+        results = None
+
+    response = {
+        "jobID": jobID,
+        "processID": PROCESS_METADATA["id"],
+        "results": results,
+        "mimetype": "application/json"
+    }
+
+    await sio.emit("simulation_results", response)
+
+    print("Job execution finished (Job ID", jobID, ")")
+
+
 # Run the main function
-asyncio.run(main())
+# Start the main function
+if __name__ == "__main__":
+    asyncio.run(main())
